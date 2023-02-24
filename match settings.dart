4ef3febea3129f1313e467b42b1a6b5330cba761
Kristen Wolovets <126212338+LateNeverdevv@users.noreply.github.com>
2023-02-24 02:38:40 +0200

    Create match settings.dart

---
+++ match settings.dart
@@ -0,0 +1,10 @@
+import 'package:fixnum/fixnum.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+import 'package:easel_flutter/utils/screen_size_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class BackgroundWidget extends StatelessWidget {
+  const BackgroundWidget({Key? key}) : super(key: key);
